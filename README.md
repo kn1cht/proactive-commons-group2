8ed8b406a6c89702432d4be27131ca606e47b9b9
kn1cht <16277200+kn1cht@users.noreply.github.com>
2019-09-30 00:21:52 +0900

    add mqtt examples

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # proactive-commons-group2
 proactive-commons-group2 project
+
+## Shiftr.io with ESP32
+Please refer to [ESP32 + Noodl + MQTTでpub/subを試してみる - Qiita](https://qiita.com/masaya-oguro/items/5def128399bde690d8a7#%E4%BD%BF%E3%81%84%E6%96%B9).
+
---
+++ esp32_mqtt_example/esp32_mqtt_example.ino
@@ -0,0 +1,49 @@
+#include <MQTT.h>
+#include <MQTTClient.h>
+#include <WiFi.h>
+
+#include "secure_config.h"
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("esp32", "3b705a7f", "b8f0aeaa652d2f08"))
+    Serial.print(".");
+
+  Serial.println("connected!");
+  client.subscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.print("topic: " + topic);
+  Serial.println("payload: " + payload);
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(messageReceived);
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(100);
+  if (!client.connected()) connect();
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
+}
